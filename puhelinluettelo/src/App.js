7856b187e1fc5d519e931ad54feaddb4c5954ca8
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-17 21:12:54 +0300

    2.11

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react' 
+import axios from 'axios'
 
 const AddPerson = (props) => {
   const state = props.state
@@ -74,29 +75,20 @@ class App extends React.Component {
   constructor(props) { 
     super(props) 
     this.state = { 
-      persons: [ 
-        { 
-          name: 'Arto Hellas',
-          number: '040-123456'
-        },
-        {
-          name: 'Martti Tienari',
-          number: '040-123456'
-        },
-        {
-          name: 'Arto Järvinen',
-          number: '040-123456'
-        },
-        {
-          name: 'Lea Kutvonen',
-          number: '040-123456'
-        }
-      ], 
+      persons: [], 
       newName: '',
       newNumber: '',
       filter: ''
     } 
   } 
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        this.setState({ persons: response.data })
+      })
+  }
 
   resetState = (value) => {
     this.setState({
