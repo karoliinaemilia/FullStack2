dfe29ec0bbeaf51486221c28369298f959fcf2c3
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-17 03:03:50 +0300

    2.1

---
+++ kurssit/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react' 
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+  return (
+    <h1>{props.kurssi}</h1>
+  )
+}
+
+const Osa = (props) => {
+  return (
+    <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+  )
+}
+
+const Sisalto = (props) => {
+  const { osat } = props
+  const rivit = () => osat.map(osa => <Osa key={osa.id} osa={osa}/>)
+    return (
+      <div>
+        {rivit()}
+      </div>
+    )
+}
+
+const Kurssi = (props) => {
+  const { kurssi } = props
+  return (
+    <div>
+      <Otsikko kurssi={kurssi.nimi}/>
+      <Sisalto osat={kurssi.osat}/>
+    </div>
+  )
+}
+
+const App = () => { 
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys', 
+    osat: [
+      {
+        nimi: 'Reactin perusteet', 
+        tehtavia: 10,
+        id: 1
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla', 
+        tehtavia: 7,
+        id: 2
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id:3
+      },
+      {
+        nimi: 'Redux',
+        tehtavia: 7,
+        id: 4
+      }
+    ]
+  } 
+  
+  return ( 
+    <div> 
+      <Kurssi kurssi={kurssi} />
+    </div> 
+  ) 
+} 
+
+ReactDOM.render( 
+  <App />, 
+  document.getElementById('root') 
+)
